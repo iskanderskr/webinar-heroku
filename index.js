016562712654eb544c5de476cb24120a38266380
iskander bittencourt <iskander.bittencourt@ativo.com>
2020-12-17 13:09:40 -0300

    app to heroku

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-import http from 'http'
+const express = require('express')
+const { Client } = require('pg')
+const fs = require('fs')
+const env = require('dotenv').config()
+
+const app = express()
+
+const getConn = () => {
+    return new Client({
+        connectionString: env.parsed.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+    });
+}
+
+app.get('/', (req, res)=>{
+    res.send('Inicio da aplicação')  
+})
+
+app.get('/contracts', (req, res)=>{
+    const client = getConn()
+    client.connect()
+    client.query(fs.readFileSync('./db.sql').toString(), (err)=>{
+        if (err) throw err
+    })
+
+    client.query('SELECT * FROM db', (err, response) => {
+        if (err) throw err
+        res.json(response.rows)
+    });
+})
+
+app.listen(5000)
